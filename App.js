4262c569370b2f958356f22c8f657728c0826da5
Frederic Jau <frederic.jau@orange.com>
2018-09-14 20:39:37 +0200

    some list

--- App.js
+++ App.js
@@ -7,24 +7,57 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import {Platform, StyleSheet, Text, View, FlatList} from 'react-native';
+import RNCalendarEvents from 'react-native-calendar-events';
 
 type Props = {};
 export default class App extends Component<Props> {
+
+  state = {
+    events: []
+  }
+
+  async componentDidMount() {
+    const startDate = new Date('2016-01-01T00:00:00');
+    const endDate = new Date('2020-12-31T23:59:59');
+  
+    await RNCalendarEvents.authorizeEventStore();
+    
+    const calendars = await RNCalendarEvents.findCalendars();
+    console.log(calendars);
+  
+    const prom = [];
+  
+    calendars.forEach((calendar) => {
+      prom.push(
+        RNCalendarEvents.fetchAllEvents(startDate, endDate, [calendar.id])
+      );
+    });
+
+    const eventsArray = await Promise.all(prom);
+    
+
+    const toto = [];
+    eventsArray.forEach((events) => {
+      console.log(events);
+      events.forEach((event) => {
+        toto.push({
+          ...event,
+          key: `${event.id}/${event.occurrenceDate}`,
+        });
+      });
+    });
+
+    this.setState({events: toto});
+  }
+
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
+      <FlatList
+        data={this.state.events}
+        renderItem={({item}) => <Text>{item.title}</Text>}
+      />    
     );
   }
 }
